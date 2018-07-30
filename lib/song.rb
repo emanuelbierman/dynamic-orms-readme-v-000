4705f3e2de276a263c4bbd269a8dc15c3d31834e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-30 11:45:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,6 @@ class Song
   end
 
   def self.find_by_name(name)
-    DB[:conn].results_as_hash = true
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
